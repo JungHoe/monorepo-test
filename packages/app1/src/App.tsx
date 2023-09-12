de906a17793e5094824caeb0aa9b4c5913fc7248
이JungHoe <junghwi0603@gmail.com>
2023-09-12 12:01:53 +0900

    Feature:app 에서 모노레포 모듈 읽기

--- packages/app1/src/App.tsx
+++ packages/app1/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
+import { Dropdown } from "@monorepo-test/common"
 import "./App.css"
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Dropdown id="app dropdown"></Dropdown>
     </>
   )
 }
